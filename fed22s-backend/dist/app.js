2bf2682fe39dd18a31eb012d9962abafef98ff7a
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-06-01 09:42:22 +0200

    (build) add functioning app and env

--- fed22s-backend/dist/app.js
+++ fed22s-backend/dist/app.js
@@ -1,30 +1,28 @@
+"use strict";
 // require("dotenv").config();
 // require("express-async-errors");
 // const express = require("express");
 // const mongoose = require("mongoose");
 // const apiRoutes = require("./routes/api");
 // const { errorMiddleware } = require("./middleware/errorMiddleware");
-
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
 // const app = express();
-
 // app.use(express.json());
-
 // app.use((req, res, next) => {
 //   console.log(`Processing ${req.method} request to ${req.path}`);
 //   next();
 // });
-
 // app.use("/api/v1", apiRoutes);
-
 // app.use(errorMiddleware);
-
 // const port = process.env.PORT || 2525;
 // const run = async () => {
 //   try {
 //     mongoose.set("strictQuery", false);
 //     const conn = await mongoose.connect(process.env.MONGODB);
 //     console.log(`MongoDB connected: ${conn.connection.host}`);
-
 //     app.listen(port, () => {
 //       console.log(
 //         `Server is listening on ${
@@ -36,21 +34,15 @@
 //     console.error(error);
 //   }
 // };
-
 // run();
-
-import express, { Express, Request, Response } from "express";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const app: Express = express();
+const express_1 = __importDefault(require("express"));
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+const app = (0, express_1.default)();
 const port = process.env.PORT || 2525;
-
-app.get("/", (req: Request, res: Response) => {
-  res.send("express working w ts");
+app.get("/", (req, res) => {
+    res.send("express working w ts");
 });
-
 app.listen(port, () => {
-  console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
+    console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
 });
--- fed22s-backend/routes/index.ts
+++
@@ -1,9 +0,0 @@
-const router = express.Router();
-
-const cartRoutes = require("./cartRoutes");
-const productRoutes = require("./productRoutes");
-
-router.use("/cart", cartRoutes);
-router.use("/products", productRoutes);
-
-module.exports = router;
