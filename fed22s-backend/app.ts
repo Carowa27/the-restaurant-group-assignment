2a0c64c7fc82dc5859b5881cb0c1e139630127cb
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-06-01 11:34:16 +0200

    (build) add app

--- fed22s-backend/app.ts
+++ fed22s-backend/app.ts
@@ -41,15 +41,38 @@
 
 import express, { Express, Request, Response } from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose";
 
 dotenv.config();
 
 const app: Express = express();
 const port = process.env.PORT || 2525;
+const connectionString = process.env.MONGO_CONNECTION_STRING;
 
-app.get("/", (req: Request, res: Response) => {
-  res.send("express working w ts");
+app.use(express.json());
+
+app.use((req, res, next) => {
+  console.log(`Processing ${req.method} request to ${req.path}`);
+
+  next();
 });
+// app.use("/api/v1/example", exampleRoutes);
+
+async function run() {
+  try {
+    mongoose.set("strictQuery", false);
+    const connectToDB = await mongoose.connect(connectionString as string);
+    console.log(`MongoDB connected: ${connectToDB.connection.host}`);
+
+    app.listen(port, () => {
+      console.log(`Server running on http://localhost:${port}`);
+    });
+  } catch (error) {
+    console.error(error);
+  }
+}
+
+run();
 
 app.listen(port, () => {
   console.log(`⚡️[server]: Server is running at http://localhost:${port}`);
