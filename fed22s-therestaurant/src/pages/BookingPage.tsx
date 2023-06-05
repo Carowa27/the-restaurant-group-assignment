954cb08b8e41b20db68159efc291e1ddd30460c8
Carolina Warntorp <carolina.warntorp@outlook.com>
2023-06-05 13:23:39 +0200

    (clean) clean code to merge

--- fed22s-therestaurant/src/pages/BookingPage.tsx
+++ fed22s-therestaurant/src/pages/BookingPage.tsx
@@ -1,30 +1,9 @@
-import { CreateNewBooking } from "../components/CreateNewBooking";
-import { SearchAvailabilityForm } from "../components/SearchAvailabilityForm";
-import { SittingForm } from "../components/SittingForm";
-import { InfoWrapper } from "../components/styled/Wrappers";
+import { Booking } from "../components/Booking/Booking";
 
 export const BookingPage = () => {
   return (
     <>
-      <h2>Boka bord</h2>
-      <InfoWrapper>
-        <section
-          style={{
-            display: "flex",
-            flexDirection: "column",
-          }}
-        >
-          <SearchAvailabilityForm></SearchAvailabilityForm>
-        </section>
-        <section>
-          <CreateNewBooking></CreateNewBooking>
-        </section>
-        {/*<form> <input type="text" placeholder="Förnamn" />
-        <input type="text" placeholder="Efternamn" />
-        <input type="email" placeholder="Email" />
-        <input type="number" placeholder="Telefonnummer" /> 
-      </form>*/}
-      </InfoWrapper>
+      <Booking></Booking>
     </>
   );
 };
